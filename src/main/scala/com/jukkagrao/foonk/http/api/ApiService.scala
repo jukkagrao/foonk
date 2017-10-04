daa3abfea3a93a6d1d4ee44062b9804ea561cdf4
Yuriy Gatilin <jukkagrao@gmail.com>
2017-10-04 23:35:14 +0200

    Listener entity. Fix problem with async endpoint. Listeners API endpoint. Add connection time and IP address to ListenerSerializer, connection time to MediaStreamSerializer. UTF-8 encoding for old sources. New Directives. Listeners counting.

--- src/main/scala/com/jukkagrao/foonk/http/api/ApiService.scala
+++ src/main/scala/com/jukkagrao/foonk/http/api/ApiService.scala
@@ -3,30 +3,58 @@ package com.jukkagrao.foonk.http.api
 import javax.ws.rs.Path
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
-import com.jukkagrao.foonk.db.StreamDb
-import com.jukkagrao.foonk.http.api.serializers.{MediaStreamSerializer, MediaStreamsSerializer}
+import akka.http.scaladsl.server.Route
+import com.jukkagrao.foonk.db.{ListenerDb, StreamDb}
+import com.jukkagrao.foonk.http.api.serializers.{ListenerSerializer, MediaStreamInfoSerializer, MediaStreamSerializer, MediaStreamsSerializer}
+import com.jukkagrao.foonk.http.directives.Directives._
 import io.swagger.annotations.{Api, ApiOperation, ApiResponse, ApiResponses}
 import spray.json._
 
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val streamFormat: RootJsonFormat[MediaStreamSerializer] = jsonFormat3(MediaStreamSerializer.apply)
-  implicit val streamsFormat: RootJsonFormat[MediaStreamsSerializer] = jsonFormat1(MediaStreamsSerializer)
+  implicit val streamFormat = jsonFormat9(MediaStreamSerializer.apply)
+  implicit val streamsFormat = jsonFormat1(MediaStreamsSerializer)
+  implicit val listenerFormat = jsonFormat3(ListenerSerializer.apply)
+  implicit val streamInfoFormat = jsonFormat2(MediaStreamInfoSerializer.apply)
 }
 
 @Api(value = "/api", description = "", produces = "application/json")
 @Path("/api")
 object ApiService extends JsonSupport {
 
-  val route: Route = path("api" / "streams")(getAll)
+  val route: Route = pathPrefix("api" / "streams") {
+    pathEnd(getAll) ~ getInfo
+  }
 
   @ApiOperation(value = "Return listing of streams", notes = "", nickname = "streams_all", httpMethod = "GET")
   @ApiResponses(Array(
     new ApiResponse(code = 200, message = "List of Streams", response = classOf[MediaStreamsSerializer])
   ))
   def getAll: Route = get {
-    complete(MediaStreamsSerializer(StreamDb.all.map(s => MediaStreamSerializer(s._1, s._2))))
+    utf8json {
+      complete(MediaStreamsSerializer(StreamDb.all.map { case (_, stream) =>
+        MediaStreamSerializer(stream, ListenerDb.countByPath(stream.path))
+      }))
+    }
   }
+
+  @ApiOperation(value = "Return info about stream", notes = "", nickname = "stream_info", httpMethod = "GET")
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Stream Info", response = classOf[MediaStreamInfoSerializer])
+  ))
+  def getInfo: Route = streamPath { sPath =>
+    get {
+      utf8json {
+        complete(streamInfo(sPath))
+      }
+    }
+  }
+
+  private def streamInfo(path: String) = StreamDb.get(path).map(stream =>
+    MediaStreamInfoSerializer((stream,
+      ListenerDb.getByPath(path).map { case (_, listener) =>
+        ListenerSerializer(listener)
+      }))
+  )
 }
