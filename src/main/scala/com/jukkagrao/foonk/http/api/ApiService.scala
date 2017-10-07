ed2e8feaa70b46615c4b9dc4d52e4a3517f56d34
Yuriy Gatilin <jukkagrao@gmail.com>
2017-10-08 00:31:30 +0200

    Added Listener kick api call

--- src/main/scala/com/jukkagrao/foonk/http/api/ApiService.scala
+++ src/main/scala/com/jukkagrao/foonk/http/api/ApiService.scala
@@ -2,23 +2,30 @@ package com.jukkagrao.foonk.http.api
 
 import javax.ws.rs.Path
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.jukkagrao.foonk.db.{ListenerDb, StreamDb}
 import com.jukkagrao.foonk.http.api.serializers.{ListenerSerializer, MediaStreamInfoSerializer, MediaStreamSerializer, MediaStreamsSerializer}
 import com.jukkagrao.foonk.http.directives.Directives._
-import io.swagger.annotations.{Api, ApiOperation, ApiResponse, ApiResponses}
-
+import io.swagger.annotations._
 
 
 @Api(value = "/api", description = "", produces = "application/json")
 @Path("/api")
 object ApiService {
 
-  val route: Route = pathPrefix("api" / "streams") {
-    pathEnd(getAll) ~ getInfo
+  val route: Route = pathPrefix("api") {
+    pathPrefix("streams") {
+      pathEnd(getAll) ~
+        getInfo
+    } ~ pathPrefix("listeners") {
+      kickListener
+    }
   }
 
+
+  @Path("/streams")
   @ApiOperation(value = "Return listing of streams", notes = "", nickname = "streams_all", httpMethod = "GET")
   @ApiResponses(Array(
     new ApiResponse(code = 200, message = "List of Streams", response = classOf[MediaStreamsSerializer])
@@ -31,9 +38,15 @@ object ApiService {
     }
   }
 
+
+  @Path("/streams/{stream}")
   @ApiOperation(value = "Return info about stream", notes = "", nickname = "stream_info", httpMethod = "GET")
   @ApiResponses(Array(
-    new ApiResponse(code = 200, message = "Stream Info", response = classOf[MediaStreamInfoSerializer])
+    new ApiResponse(code = 200, message = "Stream Info", response = classOf[MediaStreamInfoSerializer]),
+    new ApiResponse(code = 404, message = "Stream not found")
+  ))
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "stream", value = "Stream path", required = true, dataType = "string", paramType = "path")
   ))
   def getInfo: Route = streamPath { sPath =>
     get {
@@ -49,4 +62,25 @@ object ApiService {
         ListenerSerializer(listener)
       }))
   )
+
+
+  @Path("/listeners/{id}")
+  @ApiOperation(value = "Kick listener", notes = "", nickname = "kick_listener", httpMethod = "DELETE")
+  @ApiResponses(Array(
+    new ApiResponse(code = 204, message = "Listener kicked"),
+    new ApiResponse(code = 404, message = "Listener not found")
+  ))
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "id", value = "Listener Id", required = true, dataType = "integer", paramType = "path")
+  ))
+  def kickListener: Route = path("listeners" / IntNumber) { id =>
+    delete {
+      ListenerDb.get(id) match {
+        case Some(listener) =>
+          listener.kill()
+          complete(StatusCodes.NoContent)
+        case None => complete(StatusCodes.NotFound)
+      }
+    }
+  }
 }
